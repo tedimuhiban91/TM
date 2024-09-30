7be184c0a044253b440a45a4eee0b039ad553e60
tedimuhiban91 <143668183+tedimuhiban91@users.noreply.github.com>
2024-09-30 04:39:01 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true, 
+      padding: '16px',
+    },
+
+    extend: {
+      colors: { 
+        primary: '#0d9488',
+        secondary: '#475569',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
+
